d0f6742d02f4932ce5c695bcf52a7d2a743b02ee
Darlysson Alves <drlssnlvs@gmail.com>
2022-03-21 05:46:53 -0300

    (feat): doc api with swagger ui

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import routes from "./routes";
+
+import swaggerConfig from "./config/swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
 app.use(routes);
 
